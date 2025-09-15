2f1feff0cb25f8415938522b65d9830f6f0c9e17
Shiv Pratap Singh Chandel <shivpratap1122005@gmail.com>
2025-09-15 20:50:41 +0530

    Bhool gaya kya change kiya

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,8 +4,13 @@ if not ok then
   return
 end
 
+-- This is the critical part - get capabilities from cmp_nvim_lsp
 local cmp_ok, cmp_lsp = pcall(require, 'cmp_nvim_lsp')
-local capabilities = cmp_ok and cmp_lsp.default_capabilities() or {}
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+if cmp_ok then
+  capabilities = cmp_lsp.default_capabilities(capabilities)
+end
 
 -- LSP keymaps on attach
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -21,6 +26,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<leader>vrn', vim.lsp.buf.rename, opts)
     vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
+    
+    -- Debug: Print when LSP attaches
+    print("LSP attached to buffer " .. ev.buf)
   end,
 })
 
@@ -28,10 +36,16 @@ vim.api.nvim_create_autocmd('LspAttach', {
 lspconfig.clangd.setup({
   capabilities = capabilities,
   cmd = { "clangd", "--background-index" },
+  on_attach = function(client, bufnr)
+    print("Clangd attached to buffer " .. bufnr)
+  end,
 })
 
--- Configure pyright for Python
+-- Configure pyright for Python  
 lspconfig.pyright.setup({
   capabilities = capabilities,
+  on_attach = function(client, bufnr)
+    print("Pyright attached to buffer " .. bufnr)
+  end,
 })
 
--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,12 +1,20 @@
 -- lua/plugins/completion.lua
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
+  {
     "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
+    version = "v2.*",
+    build = "make install_jsregexp",
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer", 
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
   },
 }
+
