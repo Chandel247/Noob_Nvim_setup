919a3fd4bf2cf6e40dd78314e38a61728809a07d
Shiv Pratap Singh Chandel <shivpratap1122005@gmail.com>
2026-06-14 15:33:07 +0530

    Integrated C/CPP compatible treesitter setup

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,8 +1,18 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "python", "lua", "cpp", "c" },
-	sync_install = false,
-	auto_install = true,
-	highlight = { enable = true },
-	indent = { enable = true },
+    ensure_installed = { "python", "lua", "cpp", "c" },
+    sync_install = false,
+    auto_install = true,
+    highlight = { enable = true },
+    indent = {
+        enable = true,
+        disable = {"c", "cpp"},
+    },
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "c", "cpp" },
+    callback = function()
+        vim.opt_local.cindent = true
+        vim.opt_local.equalprg = "clang-format"
+    end,
+})
